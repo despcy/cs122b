cc80a3b8e54ae5466a26cf356975ad6b993942dc
yangchenxi <ycx@live.cn>
2020-05-07 23:12:00 -0700

    update parser

---
+++ DataInsert/dbinsert/src/com/cs122b/project/Main.java
@@ -0,0 +1,20 @@
+package com.cs122b.project;
+
+public class Main {
+
+    //call and handle output exception
+
+    //ActorParser(genreate starID) then CastParser
+
+    //MainParser
+    public static void main(String[] args) {
+	// write your code here
+       // MainParser mainp=new MainParser();
+        //mainp.parse();
+       // CastParser castp=new CastParser();
+       // castp.parse();
+        ActorParser actorp=new ActorParser();
+        actorp.parse();
+
+    }
+}
