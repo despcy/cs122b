ec974ccaa00c4639e722d5c4aaa2f0f01f0e50ca
yangchenxi <ycx@live.cn>
2020-05-10 00:44:09 -0700

    update

--- DataInsert/dbinsert/run.sh
+++ DataInsert/dbinsert/run.sh
@@ -7,6 +7,6 @@ wget http://infolab.stanford.edu/pub/movies/actors63.xml
 wget http://infolab.stanford.edu/pub/movies/casts124.xml
 
 
-mvn exec:java -Dexec.mainClass="com.cs122b.project.Main"
+mvn exec:java -Dexec.mainClass="com.cs122b.project.Main" -Dexec.cleanupDaemonThreads=false
 
 
