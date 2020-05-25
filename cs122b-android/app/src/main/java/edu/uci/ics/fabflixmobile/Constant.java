0b6607755c8bcacb23b9117435eb51d36e7a6809
Chenxi Yang <ycx@live.cn>
2020-05-25 16:12:32 -0700

    Update Constant.java

--- cs122b-android/app/src/main/java/edu/uci/ics/fabflixmobile/Constant.java
+++ cs122b-android/app/src/main/java/edu/uci/ics/fabflixmobile/Constant.java
@@ -1,6 +1,6 @@
 package edu.uci.ics.fabflixmobile;
 
 public class Constant {
-    public static String host="http://10.0.0.223:8080";
-    public static String aws="https://18.188.106.209:8443";
+    public static String host="https://18.188.106.209:8443";
+
 }
